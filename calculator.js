073cb2adc14fdc80c4671444cc17ccc26c4ad502
unknown <preetkathuria592@gmail.com>
2026-01-19 16:37:23 +0530

    vompleting calculator project

--- calculator.js
+++ calculator.js
@@ -1,8 +1,28 @@
-const button = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 const monitor = document.querySelector(".monitor input");
+let freshstart = false;
 
-button.addeventListner("click",() => {
-    let inside = button.innerText;
-    console.log(inside);
-    monitor.innerText = inside;
-})
+buttons.forEach((button) => {
+    button.addEventListener("click" , () => {
+        let value = button.innerText;
+        console.log(value);
+        if(freshstart === true) {
+            monitor.value = value;
+            freshstart = false;
+            monitor.value = monitor.value.slice(0,-1);
+        }
+        if(!isNaN(value) || "+-*/.".includes(value)) {
+            monitor.value += value;
+        }
+        if(value === "=") {
+            monitor.value = eval(monitor.value);
+            freshstart = true;
+        }
+        if(value === "AC") {
+            monitor.value = "";
+        }
+        if(value === "DEL") {
+            monitor.value = monitor.value.slice(0,-1);
+        }
+    });
+});
